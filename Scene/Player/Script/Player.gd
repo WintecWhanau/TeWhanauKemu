05b1fe7bf1bf5977b290b8584d6639f603f94553
stevie <stevie.berryman.dev@gmail.com>
2020-05-20 01:50:32 +1200

    update: added animations
    run and jump animations

--- Scene/Player/Script/Player.gd
+++ Scene/Player/Script/Player.gd
@@ -10,17 +10,22 @@ func _physics_process(delta):
 	motion.y += gravity
 	
 	if Input.is_action_pressed("ui_right"):
+		$AnimatedSprite.play("run")
+		$AnimatedSprite.flip_h = false
 		motion.x = speed
 	elif Input.is_action_pressed("ui_left"):
+		$AnimatedSprite.play("run")
+		$AnimatedSprite.flip_h = true
 		motion.x = -speed
 	else:
+		$AnimatedSprite.play("idle")
 		motion.x = 0
 	
 	if is_on_floor():
 		if Input.is_action_pressed("ui_up"):
 			motion.y = jumpHeight
 	else:
-		#$AnimatedSprite.play("Jump")
+		$AnimatedSprite.play('jump')
 		pass
 		
 	motion = move_and_slide(motion, up)
