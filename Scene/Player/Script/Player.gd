697d5abdcbb39d89946195384577ae055cf86e9b
stevie <stevie.berryman.dev@gmail.com>
2020-06-12 10:57:33 +1200

    update: add falling animation
    base player movement

--- Scene/Player/Script/Player.gd
+++ Scene/Player/Script/Player.gd
@@ -1,34 +1,8 @@
-#func _physics_process(delta):
-#	motion.y += gravity
-#
-#	if Input.is_action_pressed("ui_right"):
-#		$AnimatedSprite.play("run")
-#		$AnimatedSprite.flip_h = false
-#		motion.x = speed
-#	elif Input.is_action_pressed("ui_left"):
-#		$AnimatedSprite.play("run")
-#		$AnimatedSprite.flip_h = true
-#		motion.x = -speed
-#	else:
-#		$AnimatedSprite.play("idle")
-#		motion.x = 0
-#
-#	if is_on_floor():
-#		if Input.is_action_pressed("ui_up"):
-#			motion.y = jumpHeight
-#	else:
-#		$AnimatedSprite.play('jump')
-#		pass
-#
-#	motion = move_and_slide(motion, up)
-##end of _physics_process
-
 extends KinematicBody2D
 class_name Player
 export (int) var speed
 export (int) var gravity
 export (int) var jumpHeight
-var motion = Vector2() # moving in 2d space
 var direction:int = 1
 var velocity := Vector2()
 const up = Vector2(0, -1)
@@ -49,7 +23,6 @@ func process_movement(delta):
 	velocity = move_and_slide(velocity,up)
 
 func process_velocity(delta):
-	"""Apply gravity"""
 	velocity.y += 20
 
 class PlayerStateMachine extends StateMachine:
