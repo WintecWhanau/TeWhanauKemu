10684b59aef2fa36bd6cd64bd26dd756e506e926
Jon Sun <jon.sun30@gmail.com>
2020-06-23 22:30:39 +1200

    Add Boss

---
+++ Scene/Enemy/AIScript/Boss.gd
@@ -0,0 +1,20 @@
+extends KinematicBody2D
+
+onready var path_follow = get_parent()
+export var speed:int = 150
+var move_directiom = 0
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _physics_process(delta):
+	MovementLoop(delta) # Replace with function body.
+
+
+func MovementLoop(delta):
+	var prepos = path_follow.get_global_position()
+	path_follow.set_offset(path_follow.get_offset() + speed * delta)
+	var pos = path_follow.get_global_position()
+	move_directiom = (pos.angle_to_point(prepos)/3.14)*180
