9c99cbd492a96a6196bc11f16d52163bfa49e073
Stevie <56899201+stevieberryman@users.noreply.github.com>
2020-06-29 00:58:49 +1200

    init: dead scene (#85)
    Re center and button functions

---
+++ Scene/Dead/Button2.gd
@@ -0,0 +1,30 @@
+extends Button
+
+# Get node
+export (NodePath) var button_path
+onready var button = get_node(button_path)
+
+func _ready():
+	#Create connection
+	button.connect("pressed", self, "on_pressed")
+	
+	#set button name
+	button.set_text("Quit")
+	
+	#set toggle name
+	button.set_toggle_mode(true)
+
+	#Disable button
+	button.set_disabled(true)
+
+#On button toggle
+func on_toggled(pressed):
+	if(pressed):
+		print("Hello!")
+	else:
+		print("Goodbye!")
+		
+#On button pressed
+func on_pressed():
+	get_tree().change_scene("res://Title/Title.tscn")
+	print("Game Over Try Again!")
