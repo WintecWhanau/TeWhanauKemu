bfe8ae8d652df1f083a56730a7118d82339a3045
James Brooks <shihingjb@gmail.com>
2020-07-03 23:07:19 +1200

    Title Scene
    Added a button for 'Controls' menu

--- Scene/Title/Button_Start.gd
+++ Scene/Title/Button_Start.gd
@@ -1,5 +1,9 @@
 extends Button
 
 func _on_Button_Start_pressed():
-# warning-ignore:return_value_discarded
 	get_tree().change_scene("res://Scene/Chief Scene/ChiefScene.tscn")
+
+func _on_Button_Controls_pressed():
+	get_tree().change_scene("res://Scene/Keyboard Controls/Keyboard Controls.tscn")
+
+
